7f6faf777b08d83271ca37f404069e85896816f7
James Baldwin <jbaldwin@gradcenter.cuny.edu>
2025-02-02 20:10:52 -0500

    fixed logger issues

--- MLP_optuna/mlp_main.py
+++ MLP_optuna/mlp_main.py
@@ -140,9 +140,7 @@ def trainMLP(config, logger, verbose):
         avg_epoch_loss = total_loss / len(config["X_train"])
         mlp_losses.append(avg_epoch_loss)
 
-        logger.report_scalar(avg_epoch_loss, epoch) # task.report_scaler
-        if trial.should_prune():
-            raise optuna.TrialPruned()
+        logger.report_scalar("avg_epoch_loss", avg_epoch_loss)
 
         if verbose and (epoch + 1) % (0.1 * config["num_epochs"]) == 0:
             print(f"Epoch [{epoch + 1}/{config['num_epochs']}], Loss: {avg_epoch_loss:.4e}")
@@ -196,11 +194,14 @@ def main():
         logger=logger,
         verbose=False
     )
+    logger.report_scalar("train_losses", losses)
 
     # test the model and report the results
     preds, test_mse = test(model, y_train, scaler)
     task.upload_artifact("preds", preds)
-    logger.report_scalar(test_mse)
+    logger.report_single_value(name="test_mse", value=test_mse)
+
+    logger.flush()
         
 if __name__ == "__main__":
     main()
