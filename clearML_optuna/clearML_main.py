b8342e66bc7b3f9da1f4873144ce938ae16a5b52
James Baldwin <jbaldwin@gradcenter.cuny.edu>
2025-02-06 08:30:46 -0500

    db

--- clearML_optuna/clearML_main.py
+++ clearML_optuna/clearML_main.py
@@ -156,6 +156,7 @@ def trainKAN(config, logger, verbose):
             optimizer.zero_grad()
             print(f"DEBUG: shape of point_set before being sent to model: {np.shape(point_set)}")
             outputs = model(point_set.to(device))
+            print(f"DEBUG: shape of outputs: {np.shape(outputs)}")
             loss = criterion(outputs, target.to(device))
             loss.backward()
             optimizer.step()
